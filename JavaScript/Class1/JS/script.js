0f830d2e4bc3fd782e1575bfe44bdc3b40eca84a
Sarjak <bhandarisarjak@gmail.com>
2024-11-28 15:04:03 +0545

    JS first class

---
+++ JavaScript/Class1/JS/script.js
@@ -0,0 +1,39 @@
+console.log('iTS woRKINNN');
+var a=10;
+console.log(a);
+document.write("IT IS WORKINGGGGG YAAAY");
+// alert("Sarjak ! ITS WOOOOOKINNNNNGGGG")
+let name="ravi";
+// confirm(name+"Bhatta");
+function test(){
+    console.log(a);
+    
+}
+ name="yaay";
+document.write(name)
+a=1;
+let b=4;
+console.log(a+b);
+console.log(a*b);
+console.log(a^b);
+
+a=10;
+b=20;
+let d=30;
+let e=30;
+let f=30;
+
+var data= (a==b)||(b<a)?"true":"false";
+console.log(typeof data);
+data=Boolean(data);
+console.log(typeof data);
+
+function getGrade(){
+    var entrydata=document.getElementById("entry").value;
+    
+    console.log(entrydata)
+    if(entrydata>=80){
+        document.getElementById("output").innerHTML.entrydata;
+    }
+    
+}

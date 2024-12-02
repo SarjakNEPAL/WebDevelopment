3e9e1fd5b85c661239cb83839c71b33294cf6f2b
Sarjak <bhandarisarjak@gmail.com>
2024-12-02 07:06:25 +0545

    Loop

---
+++ Loop/forloop.js
@@ -0,0 +1,13 @@
+function startForLoop(){
+    for(var i=0; i<10; i++){
+        console.log(i);
+    }    
+}
+
+function startForLoopSum(){
+    var sum=0;
+    for(var i=0; i<10; i++){
+        sum=sum+i;
+    }
+    console.log(sum);    
+}
---
+++ Loop/whileloop.js
@@ -0,0 +1,16 @@
+function startWhile(){
+    var i=0;
+    while(i!=10){
+        console.log(i);
+        i++;
+    }
+}
+function startWhileSum(){
+    var i=0;
+    var sum=0;
+    while(i!=10){
+        sum=sum+i;
+        i++;
+    }
+    console.log(sum);
+}

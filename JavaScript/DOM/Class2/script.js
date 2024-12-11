ab4d95565c68726e03d429ad3db6a4799d9bbec1
Sarjak <bhandarisarjak@gmail.com>
2024-12-11 07:11:21 +0545

    DOM class 2

--- JavaScript/DOM/Class2/script.js
+++ JavaScript/DOM/Class2/script.js
@@ -1,6 +1,37 @@
+//constants
 const head=document.getElementById("Headingwa");
-console.log(head);
 const btn=document.getElementById("btn-wow");
+const btn_add=document.getElementById("add");
+const btn_rem=document.getElementById("rem");
+const list_add=document.getElementById("fruits");
+
+//console.logs
+console.log(head);
 btn.addEventListener("click",()=>{
     console.log("You Clicked The Button");
 })
+
+btn_add.addEventListener("click",()=>{
+    var newItem=document.createElement("li");
+    newItem.className="list-item";
+    
+    newItem.textContent=prompt("Enter Fruit Name");
+    list_add.appendChild(newItem);
+});
+
+btn_rem.addEventListener("click",()=>{
+    if(list_add.lastElementChild){
+        list_add.removeChild(list_add.lastElementChild);
+    }
+    else{
+        alert("No element left");
+    }
+});
+
+head.style.fontWeight="bold";
+head.style.color="blue";
+
+//paragrapgings
+document.write("<p id='Para'> This Is the paragraph</p>")
+document.getElementById("Para").style.fontWeight="bold";
+
